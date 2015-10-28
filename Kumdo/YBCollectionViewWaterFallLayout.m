ef1d54fce9f28031182a85190cb63e4ac42fc175
Jang Young-bin <jyb0823@gmail.com>
2015-10-28 10:45:47 +0900

    Change property to ivar in implement of YBCollectionViewWaterFallLayout

--- Kumdo/YBCollectionViewWaterFallLayout.m
+++ Kumdo/YBCollectionViewWaterFallLayout.m
@@ -8,31 +8,29 @@
 
 #import "YBCollectionViewWaterFallLayout.h"
 
-@interface YBCollectionViewWaterFallLayout()
-
-@property (nonatomic) NSDictionary *layoutInformation;
-@property (nonatomic) UIEdgeInsets sectionInset;
-@property (nonatomic) NSMutableArray *itemPositions; // item position for left column, right column.
-
-@end
 
 @implementation YBCollectionViewWaterFallLayout
 {
     __weak id <YBCollectionViewDelegateWaterFallLayout> delegate;
+    NSDictionary *mLayoutInformation;
+    UIEdgeInsets sectionInset;
+    NSMutableArray *itemPositions; // item position for left column, right column.
 }
+
+
+@synthesize delegate = delegate;
+
 
 - (instancetype)init
 {
     self = [super init];
     
     if (self) {
-        _sectionInset = UIEdgeInsetsZero;
+        sectionInset = UIEdgeInsetsZero;
     }
     
     return self;
 }
-
-@synthesize delegate = delegate;
 
 - (void)prepareLayout
 {
@@ -44,9 +42,9 @@
     NSIndexPath *indexPath;
     NSInteger numSections = [self.collectionView numberOfSections];
 
-    _itemPositions = [NSMutableArray array];
-    _itemPositions[0] = [NSValue valueWithCGPoint:CGPointMake(_sectionInset.left, _sectionInset.top)]; // 왼쪽 Column의 postion 초기화
-    _itemPositions[1] = [NSValue valueWithCGPoint:CGPointMake(_sectionInset.left, _sectionInset.top)]; // 오른쪽 Column의 position 초기화
+    itemPositions = [NSMutableArray array];
+    itemPositions[0] = [NSValue valueWithCGPoint:CGPointMake(sectionInset.left, sectionInset.top)]; // 왼쪽 Column의 postion 초기화
+    itemPositions[1] = [NSValue valueWithCGPoint:CGPointMake(sectionInset.left, sectionInset.top)]; // 오른쪽 Column의 position 초기화
     
     for (NSInteger section = 0; section < numSections; section++) {
         NSInteger numItems = [self.collectionView numberOfItemsInSection:section];
@@ -58,27 +56,27 @@
             
             CGSize itemSize = [self.delegate collectionView:self.collectionView layout:self sizeForItemAtIndexPath:indexPath]; // each cell x, y, width, height
             
-            CGPoint itemPosition = [[_itemPositions objectAtIndex:(item % 2)] CGPointValue];
+            CGPoint itemPosition = [[itemPositions objectAtIndex:(item % 2)] CGPointValue];
             itemPosition.x = itemSize.width * (item % 2); // 오른쪽 column(1)이면 item width만큼 x가 옮겨진다
             
             attributes.frame = CGRectMake(itemPosition.x, itemPosition.y, itemSize.width, itemSize.height);
             [cellInformation setObject:attributes forKey:indexPath];
             
             itemPosition.y += itemSize.height;
-            _itemPositions[item % 2] = [NSValue valueWithCGPoint:itemPosition];
+            itemPositions[item % 2] = [NSValue valueWithCGPoint:itemPosition];
         }
     }
     [layoutInformation setObject:cellInformation forKey:@"MyCellKind"];
-    self.layoutInformation = layoutInformation;
+    mLayoutInformation = layoutInformation;
 }
 
 - (CGSize)collectionViewContentSize
 {
     CGFloat height = 0;
-    if ([_itemPositions[0] CGPointValue].y > [_itemPositions[1] CGPointValue].y) {
-        height = [_itemPositions[0] CGPointValue].y;
+    if ([itemPositions[0] CGPointValue].y > [itemPositions[1] CGPointValue].y) {
+        height = [itemPositions[0] CGPointValue].y;
     } else {
-        height = [_itemPositions[1] CGPointValue].y;
+        height = [itemPositions[1] CGPointValue].y;
     }
     
     return CGSizeMake(self.collectionView.frame.size.width, height);
@@ -86,9 +84,9 @@
 
 - (NSArray<UICollectionViewLayoutAttributes *> *)layoutAttributesForElementsInRect:(CGRect)rect
 {
-    NSMutableArray *myAttributes = [NSMutableArray arrayWithCapacity:[self.layoutInformation count]];
-    for (NSString *key in self.layoutInformation) {
-        NSDictionary *attributesDict = [self.layoutInformation objectForKey:key];
+    NSMutableArray *myAttributes = [NSMutableArray arrayWithCapacity:[mLayoutInformation count]];
+    for (NSString *key in mLayoutInformation) {
+        NSDictionary *attributesDict = [mLayoutInformation objectForKey:key];
         for (NSIndexPath *key in attributesDict) {
             UICollectionViewLayoutAttributes *attributes = [attributesDict objectForKey:key];
             if (CGRectIntersectsRect(rect, attributes.frame)) {
@@ -101,7 +99,7 @@
 
 - (UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath
 {
-    return self.layoutInformation[@"MyCellKind"][indexPath];
+    return mLayoutInformation[@"MyCellKind"][indexPath];
 }
 
 @end
